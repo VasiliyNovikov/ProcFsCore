cb295e8ef0f0d46f23fe67ee20dc85fad23491e2
vasiliy <vasiliy@ubuntu18-dev.mshome.net>
2019-12-27 11:55:32 +0200

    Disk tests - mode debug info

--- ProcFsCore.Tests/DiskStatisticsTests.cs
+++ ProcFsCore.Tests/DiskStatisticsTests.cs
@@ -15,9 +15,9 @@ namespace ProcFsCore.Tests
                 
                 void Verify(in DiskStatistics.Operation op)
                 {
-                    Assert.IsTrue(op.Count > 0, "Count > 0");
-                    Assert.IsTrue(op.Bytes > 0, "Bytes > 0");
-                    Assert.IsTrue(op.Time > 0, "Time > 0");
+                    Assert.IsTrue(op.Count > 0, $"{stat.DeviceName} Count > 0");
+                    Assert.IsTrue(op.Bytes > 0, $"{stat.DeviceName} Bytes > 0");
+                    Assert.IsTrue(op.Time > 0, $"{stat.DeviceName} Time > 0");
                 }
                 
                 Verify(stat.Reads);
@@ -25,12 +25,12 @@ namespace ProcFsCore.Tests
                 if (stat.TotalTime > 0)
                 {
                     hasTotalTime = true;
-                    Assert.IsTrue(stat.TotalWeightedTime >= stat.TotalTime, "TotalWeightedTime >= TotalTime");
-                    Assert.AreEqual(stat.Reads.Time + stat.Writes.Time, stat.TotalWeightedTime, 0.5);
+                    Assert.IsTrue(stat.TotalWeightedTime >= stat.TotalTime, $"{stat.DeviceName} TotalWeightedTime >= TotalTime");
+                    Assert.AreEqual(stat.Reads.Time + stat.Writes.Time, stat.TotalWeightedTime, 0.5, $"{stat.DeviceName} Reads.Time + Writes.Time ≈ TotalWeightedTime");
                 }
             }
 
-            Assert.IsTrue(hasTotalTime, "TotalTime > 0 for at least one disk");
+            Assert.IsTrue(hasTotalTime, "TotalTime > 0 for at least one device");
         }
     }
 }
