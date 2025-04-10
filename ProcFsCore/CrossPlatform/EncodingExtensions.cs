285183e1eb36b2fecff62a348abf20bf6dc284b9
netem <vanoviko@microsoft.com>
2025-04-10 13:15:55 +0000

    Fix

--- ProcFsCore/CrossPlatform/EncodingExtensions.cs
+++ ProcFsCore/CrossPlatform/EncodingExtensions.cs
@@ -7,12 +7,16 @@ internal static class EncodingExtensions
 {
     public static unsafe string GetString(this Encoding encoding, ReadOnlySpan<byte> bytes)
     {
+        if (bytes.IsEmpty)
+            return string.Empty;
         fixed (byte* bytesPtr = &MemoryMarshal.GetReference(bytes))
             return encoding.GetString(bytesPtr, bytes.Length);
     }
 
     public static unsafe int GetChars(this Encoding encoding, ReadOnlySpan<byte> bytes, Span<char> chars)
     {
+        if (bytes.IsEmpty)
+            return 0;
         fixed (byte* bytesPtr = &MemoryMarshal.GetReference(bytes))
         fixed (char* charsPtr = &MemoryMarshal.GetReference(chars))
             return encoding.GetChars(bytesPtr, bytes.Length, charsPtr, chars.Length);
