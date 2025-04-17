ba3aecaf6062a4cfdbf3c566e56a7ba9ea3243a4
Vasyl Novikov <vanoviko@microsoft.com>
2025-04-17 17:46:12 +0000

    Edits

--- ProcFsCore/CrossPlatform/DateTimeExtensions.cs
+++ ProcFsCore/CrossPlatform/DateTimeExtensions.cs
@@ -4,6 +4,7 @@ namespace System;
 internal static class DateTimeExtensions
 {
     private static readonly DateTime UnuxEpochValue = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
     extension(DateTime)
     {
         public static DateTime UnixEpoch => UnuxEpochValue;
